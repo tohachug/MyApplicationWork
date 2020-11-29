32fc2f717c266649bb67b8d954de4fe9034855a5
toha <toha@av-prikamie.ru>
2020-11-29 21:28:55 +0500

    make homework3

--- app/src/main/java/com/example/myapplicationwork/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationwork/MainActivity.kt
@@ -4,10 +4,28 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() , FragmentMoviesList.TransactionsFragmentClicks {
+
+    private val moviesListFragment = FragmentMoviesList().apply { setClickListener(this@MainActivity) };
+    private val moviesDetailsFragment = FragmentMoviesDetails();
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if(savedInstanceState == null)
+            supportFragmentManager.beginTransaction()
+                .apply {
+                    //add(R.id.main_container, moviesDetailsFragment)
+                    add(R.id.main_container, moviesListFragment)
+                    commit()
+                }
+    }
+
+    override fun addDetailsFragment() {
+         supportFragmentManager.beginTransaction()
+            .apply {
+                replace(R.id.main_container, FragmentMoviesDetails.newInstance("1","1")).addToBackStack(null)
+                commit()
+            }
     }
 }
